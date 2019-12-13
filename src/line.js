7ee236f03d7cf197d9c846198c77333f10269295
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2019-12-13 16:03:41 +0530

    modified Line component.

--- src/line.js
+++ src/line.js
@@ -1,25 +1,21 @@
 const arePointsEqual = function(pointA, pointB) {
 	return pointA.x === pointB.x && pointA.y === pointB.y;
 };
-const isTypeEqual = function(obj1, obj2) {
-	return obj1.constructor.name === obj2.constructor.name;
-};
 
 class Line {
 	constructor(pointA, pointB) {
 		this.endA = { x: pointA.x, y: pointA.y };
-		this.endB = { x: pointB, y: pointB.y };
+		this.endB = { x: pointB.x, y: pointB.y };
 	}
 	toString() {
-		let type = this.constructor.name;
-		return `${type} (${this.endA.x},${this.endA.y}), (${this.endB.x},${this.endB.y})`;
+		return `Line (${this.endA.x},${this.endA.y}), (${this.endB.x},${this.endB.y})`;
 	}
 
-	isEqualTo(otherLine) {
+	isEqualTo(other) {
+		if (!other instanceof Line) return false;
 		return (
-			isTypeEqual(this, otherLine) &&
-			arePointsEqual(this.endA, otherLine.endA) &&
-			arePointsEqual(this.endB, otherLine.endB)
+			arePointsEqual(this.endA, other.endA) &&
+			arePointsEqual(this.endB, other.endB)
 		);
 	}
 }
