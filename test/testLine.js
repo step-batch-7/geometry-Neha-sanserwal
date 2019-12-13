2b3cf49de170db58cbc2d20393495c05128cab28
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2019-12-13 17:36:37 +0530

    wriiten more tests for property length of class line

--- test/testLine.js
+++ test/testLine.js
@@ -1,4 +1,4 @@
-const assert = require("assert");
+const assert = require("chai").assert;
 const Line = require("../src/line.js").Line;
 
 describe("Line", function() {
@@ -31,6 +31,14 @@ describe("Line", function() {
 		it("should calculate for line having same end points", function() {
 			const line = new Line({ x: 1, y: 2 }, { x: 1, y: 2 });
 			assert.strictEqual(line.length, 0);
+		});
+		it("should calculate for line having different end points", function() {
+			const line = new Line({ x: 1, y: 2 }, { x: 2, y: 2 });
+			assert.strictEqual(line.length, 1);
+		});
+		it("should calculate for line having points of floating type", function() {
+			const line = new Line({ x: 1.3, y: 2.0 }, { x: 2.1, y: 2.54 });
+			assert.approximately(line.length, 0.96, 0.01);
 		});
 	});
 });
